90c669707e6cdf703123592213e3e808e9aa093b
felipesantos <felipesantosswengineer@gmail.com>
2021-05-10 18:48:43 -0300

    1.01

--- src/main/kotlin/me/felipesantos/Application.kt
+++ src/main/kotlin/me/felipesantos/Application.kt
@@ -8,9 +8,46 @@ fun main(){
 
     val my_name_val = "felipe"
     var my_name_var = "santos"
-    println(my_name_val + " " + my_name_var)
-    my_name_var = "rodrigues"
-    println(my_name_var)
 
+    println(my_name_val + " " + my_name_var)
+
+    //? na declaracao com tipo esperado define se a variavel pode ser nula
+    val nao_nulo: String?
+    nao_nulo = null
+
+    //? para previnir null point exc
+    println(nao_nulo?.length)
+
+    // or de maneira classica
+    val nulo = null
+    if(nulo == null)
+        println("vazio")
+    else
+        println(nulo)
+
+    //or kotlin away
+    val valor: String?
+    valor = null
+
+    println(valor?.length ?: "vazio")
+
+    /*
+    Estruturas de decisoes
+     */
+
+    var resultado: Boolean
+    resultado = false
+
+    if(1 > 2){
+        resultado = true
+    }
+    else if (2 >= 3){
+        resultado = true
+    }
+    else{
+        resultado = false
+    }
+
+    println(resultado.toString())
 
 }
