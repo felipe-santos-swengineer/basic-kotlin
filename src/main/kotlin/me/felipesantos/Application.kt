b6912424fe04924aaf705ec83823a016344c2b62
felipesantos <felipesantosswengineer@gmail.com>
2021-05-10 18:06:42 -0300

    1.0

---
+++ src/main/kotlin/me/felipesantos/Application.kt
@@ -0,0 +1,16 @@
+package me.felipesantos
+
+fun main(){
+    println("meu primeiro programa kotlin")
+
+    //var = pode ser reasinada,alterada, mutavel
+    //val = imutavel (nao pode ser instanciado vazio)
+
+    val my_name_val = "felipe"
+    var my_name_var = "santos"
+    println(my_name_val + " " + my_name_var)
+    my_name_var = "rodrigues"
+    println(my_name_var)
+
+
+}
